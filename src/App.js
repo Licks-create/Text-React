729680229fa05fc5ea329ff4382ae0a121f9dd82
viveka <goluojha13101992@gmail.com>
2023-01-15 10:46:52 +0530

    savedAsApp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import Navbar from './Components/Navbar';
+import About from './Components/About';
+import TextForm from './Components/TextForm';
+// import React from 'react'
+function Appa() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <h1>my name is First{1+2+name}</h1>
+    <>
+   
+<Navbar title={"TxtUtils"} aboutNav="About"/>
+{/* <Navbar /> */}
+<div className="container">
+  <TextForm/>
+<About/>
+</div>
+    </>
   );
 }
 
-export default App;
+export default Appa;
